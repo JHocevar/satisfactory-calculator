acc371057c8d3cbce3f67d8f2f1baf672073a0fc
Jonathan Hocevar <cs6alto@verizon.net>
2019-12-16 21:48:03 -0600

    Updates

---
+++ precache-manifest.8b5cc2059eb80c6c4f3c6eb37be132ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1784502a8570387afd040ae15f6a836c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f19dbd1c566d51b60429",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "e603ff2ad02be564f874",
+    "url": "/static/css/main.5d34f35a.chunk.css"
+  },
+  {
+    "revision": "f19dbd1c566d51b60429",
+    "url": "/static/js/2.1b1f99fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1b1f99fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e603ff2ad02be564f874",
+    "url": "/static/js/main.c2e0a8e5.chunk.js"
+  },
+  {
+    "revision": "a288d0b859299193a4fc",
+    "url": "/static/js/runtime-main.aab28843.js"
+  }
+]);
